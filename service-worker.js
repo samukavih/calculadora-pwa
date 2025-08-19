6c2808081e708a805d8e7e0ccd4de86cda758014
samuakvih <samukavih4@gmail.com>
2025-08-19 13:46:00 -0300

    Atualiza PWA com ícones e JS funcional

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "calc-pwa-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Se você colocou o JS da calculadora em arquivo separado, adicione aqui
+// "./script.js"  
+
+// Instalação do service worker e cache dos arquivos
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Ativação do service worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições e retorna do cache se existir
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
